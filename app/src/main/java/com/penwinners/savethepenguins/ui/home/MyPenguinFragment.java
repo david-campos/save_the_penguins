734b82ac3ace991b68a17306d95d127cd10df491
david-campos <david.campos.r96@gmail.com>
2019-09-29 01:56:59 +0200

    Name change

--- app/src/main/java/com/penwinners/savethepenguins/ui/home/MyPenguinFragment.java
+++ app/src/main/java/com/penwinners/savethepenguins/ui/home/MyPenguinFragment.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.penwinners.savethepenguins.R;
 
 import java.util.Locale;
@@ -25,7 +26,7 @@ public class MyPenguinFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_my_penguin, container, false);
+        final View root = inflater.inflate(R.layout.fragment_my_penguin, container, false);
         SharedPreferences sharedPref = getContext().getSharedPreferences(
                 getString(R.string.prf_prefs_key), Context.MODE_PRIVATE);
         int selected = sharedPref.getInt(getString(R.string.prf_selected_penguin), 0);
@@ -34,6 +35,34 @@ public class MyPenguinFragment extends Fragment {
         ((ProgressBar) root.findViewById(R.id.xp_bar)).setProgress(progress);
         ((TextView) root.findViewById(R.id.percentage)).setText(
                 String.format(Locale.ENGLISH, "%d%%", progress));
+        String name = sharedPref.getString(getString(R.string.prf_penguin_name), null);
+        final TextInputEditText textInputEditText = root.findViewById(R.id.name_input);
+        final TextView penguinName = root.findViewById(R.id.penguin_name);
+        if (name == null) {
+            penguinName.setVisibility(View.INVISIBLE);
+            root.findViewById(R.id.btn_save_name).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String name = textInputEditText.getText().toString();
+                    if (name != null) {
+                        SharedPreferences sharedPreferences = getContext().getSharedPreferences(
+                                getString(R.string.prf_prefs_key), Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(
+                                v.getContext().getResources().getString(R.string.prf_penguin_name),
+                                name);
+                        editor.apply();
+                        editor.commit();
+                        root.findViewById(R.id.name_input_layout).setVisibility(View.INVISIBLE);
+                        penguinName.setText(name);
+                        penguinName.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        } else {
+            penguinName.setText(name);
+            root.findViewById(R.id.name_input_layout).setVisibility(View.INVISIBLE);
+        }
         return root;
     }
 }
