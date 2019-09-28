cf66dc93f8840ccb58977ead9713d3b21d6dde7a
david-campos <david.campos.r96@gmail.com>
2019-09-28 21:58:26 +0200

    Ya elige el fragment correctamente

---
+++ app/src/main/java/com/penwinners/savethepenguins/ui/home/MyPenguinFragment.java
@@ -0,0 +1,56 @@
+package com.penwinners.savethepenguins.ui.home;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.penwinners.savethepenguins.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MyPenguinFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MyPenguinFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyPenguinFragment extends Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    public MyPenguinFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MyPenguinFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyPenguinFragment newInstance(String param1, String param2) {
+        MyPenguinFragment fragment = new MyPenguinFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_penguin, container, false);
+    }
+}
