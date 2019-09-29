e315bf4e8b2091e37a7b0f2cc64bbfc19ac50443
david-campos <david.campos.r96@gmail.com>
2019-09-29 05:02:58 +0200

    Spelling mistake in achievements

--- app/src/main/java/com/penwinners/savethepenguins/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/penwinners/savethepenguins/ui/notifications/NotificationsFragment.java
@@ -5,21 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.*;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.penwinners.savethepenguins.R;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
 
